2cd57915a70c60f1908e258a569921b89798d452
lisiant <wkdxod321@gmail.com>
2024-10-17 21:44:12 +0900

    project init

---
+++ src/main/java/com/example/sqdemo/SqdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.sqdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqdemoApplication.class, args);
+	}
+
+}
